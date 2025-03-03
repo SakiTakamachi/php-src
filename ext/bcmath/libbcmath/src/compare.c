cfb191a9b219a621344eb4997fa8edf657c1cdb0
Saki Takamachi <saki@php.net>
2025-03-03 19:17:17 +0900

    modified compare

--- ext/bcmath/libbcmath/src/compare.c
+++ ext/bcmath/libbcmath/src/compare.c
@@ -34,15 +34,30 @@
 #include "private.h"
 #include <stddef.h>
 
+static inline bcmath_compare_result bc_compare_determine_return_val(bool check, sign sign, bool use_sign)
+{
+	if (check) {
+		/* Magnitude of n1 > n2. */
+		if (!use_sign || sign == PLUS) {
+			return BCMATH_LEFT_GREATER;
+		} else {
+			return BCMATH_RIGHT_GREATER;
+		}
+	} else {
+		/* Magnitude of n1 < n2. */
+		if (!use_sign || sign == PLUS) {
+			return BCMATH_RIGHT_GREATER;
+		} else {
+			return BCMATH_LEFT_GREATER;
+		}
+	}
+}
 
 /* Compare two bc numbers.  Return value is 0 if equal, -1 if N1 is less
    than N2 and +1 if N1 is greater than N2.  If USE_SIGN is false, just
    compare the magnitudes. */
-
 bcmath_compare_result _bc_do_compare(bc_num n1, bc_num n2, size_t scale, bool use_sign)
 {
-	char *n1ptr, *n2ptr;
-
 	/* First, compare signs. */
 	if (use_sign && n1->n_sign != n2->n_sign) {
 		/*
@@ -51,7 +66,7 @@ bcmath_compare_result _bc_do_compare(bc_num n1, bc_num n2, size_t scale, bool us
 		 */
 		if ((n1->n_scale > scale || n2->n_scale > scale) &&
 			n1->n_len == 1 && n2->n_len == 1 &&
-			n1->n_value[0] == 0 && n2->n_value[0] == 0 &&
+			n1->n_vectors[0] == 0 && n2->n_vectors[0] == 0 &&
 			bc_is_zero_for_scale(n1, scale) && bc_is_zero_for_scale(n2, scale)
 		) {
 			/* e.g. 0.00 <=> -0.00 */
@@ -67,85 +82,93 @@ bcmath_compare_result _bc_do_compare(bc_num n1, bc_num n2, size_t scale, bool us
 	}
 
 	/* Now compare the magnitude. */
-	if (n1->n_len != n2->n_len) {
-		if (n1->n_len > n2->n_len) {
-			/* Magnitude of n1 > n2. */
-			if (!use_sign || n1->n_sign == PLUS) {
-				return BCMATH_LEFT_GREATER;
-			} else {
-				return BCMATH_RIGHT_GREATER;
-			}
-		} else {
-			/* Magnitude of n1 < n2. */
-			if (!use_sign || n1->n_sign == PLUS) {
-				return BCMATH_RIGHT_GREATER;
-			} else {
-				return BCMATH_LEFT_GREATER;
+	if (n1->n_int_vsize != n2->n_int_vsize) {
+		return bc_compare_determine_return_val(n1->n_int_vsize > n2->n_int_vsize, n1->n_sign, use_sign);
+	}
+
+	size_t num_min_scale;
+	size_t num_max_scale;
+	size_t min_frac_vsize;
+	size_t max_frac_vsize;
+	size_t int_vsize = n1->n_int_vsize; // n1->n_int_vsize == n2->n_int_vsize
+	if (n1->n_scale > n2->n_scale) {
+		num_min_scale = n2->n_scale;
+		num_max_scale = n1->n_scale;
+		min_frac_vsize = n2->n_frac_vsize;
+		max_frac_vsize = MIN(scale / BC_VECTOR_SIZE, n1->n_frac_vsize);
+	} else {
+		num_min_scale = n1->n_scale;
+		num_max_scale = n2->n_scale;
+		min_frac_vsize = n1->n_frac_vsize;
+		max_frac_vsize = MIN(scale / BC_VECTOR_SIZE, n2->n_frac_vsize);
+	}
+
+	size_t check_scale;
+	size_t check_vsize;
+	if (num_min_scale <= scale) {
+		check_scale = num_min_scale;
+		check_vsize = int_vsize + min_frac_vsize;
+	} else {
+		check_scale = scale;
+		check_vsize = int_vsize + check_scale / BC_VECTOR_SIZE;
+	}
+
+	BC_VECTOR *n1ptr = BC_VECTORS_UPPER_PTR(n1);
+	BC_VECTOR *n2ptr = BC_VECTORS_UPPER_PTR(n2);
+
+	size_t count = check_vsize;
+	for (; count > 0; count--) {
+		if (*n1ptr != *n2ptr) {
+			return bc_compare_determine_return_val(*n1ptr > *n2ptr, n1->n_sign, use_sign);
+		}
+		n1ptr--;
+		n2ptr--;
+	}
+
+	size_t protruded_scale = BC_PROTRUNDED_LEN_FROM_LEN(scale);
+
+	if (num_min_scale > scale) {
+		/**
+		 * scale < num_min_scale <= num_max_scale
+		 * Compare protruded scale. Since the scale to be checked for n1 and n2 is the same,
+		 * the check is completed at the same time.
+		 */
+		if (protruded_scale > 0) {
+			BC_VECTOR n1_tmp = BC_EXTRACT_UPPER_DIGIT(*n1ptr, protruded_scale);
+			BC_VECTOR n2_tmp = BC_EXTRACT_UPPER_DIGIT(*n2ptr, protruded_scale);
+
+			if (n1_tmp != n2_tmp) {
+				return bc_compare_determine_return_val(n1_tmp > n2_tmp, n1->n_sign, use_sign);
 			}
 		}
+		return BCMATH_EQUAL;
 	}
 
-	size_t n1_scale = MIN(n1->n_scale, scale);
-	size_t n2_scale = MIN(n2->n_scale, scale);
-
-	/* If we get here, they have the same number of integer digits.
-	   check the integer part and the equal length part of the fraction. */
-	size_t count = n1->n_len + MIN (n1_scale, n2_scale);
-	n1ptr = n1->n_value;
-	n2ptr = n2->n_value;
-
-	while ((count > 0) && (*n1ptr == *n2ptr)) {
-		n1ptr++;
-		n2ptr++;
-		count--;
+	if (num_min_scale == num_max_scale) {
+		/* num_min_scale == num_max_scale <= scale */
+		return BCMATH_EQUAL;
 	}
 
-	if (count != 0) {
-		if (*n1ptr > *n2ptr) {
-			/* Magnitude of n1 > n2. */
-			if (!use_sign || n1->n_sign == PLUS) {
-				return BCMATH_LEFT_GREATER;
-			} else {
-				return BCMATH_RIGHT_GREATER;
-			}
-		} else {
-			/* Magnitude of n1 < n2. */
-			if (!use_sign || n1->n_sign == PLUS) {
-				return BCMATH_RIGHT_GREATER;
-			} else {
-				return BCMATH_LEFT_GREATER;
-			}
+	/**
+	 * Checking for shorter num is done.
+	 * Scales of n1 and n2 are not equal, check whether the longer value is 0.
+	 */
+	count = int_vsize + max_frac_vsize - check_vsize;
+	BC_VECTOR *n_ex_ptr = n1->n_scale > n2->n_scale ? n1ptr : n2ptr;
+	for (; count > 0; count--) {
+		if (*n_ex_ptr != 0) {
+			return bc_compare_determine_return_val(n1->n_scale > n2->n_scale, n1->n_sign, use_sign);
+		}
+		n_ex_ptr--;
+	}
+
+	/* num_min_scale <= scale < num_max_scale, so last check */
+	if (protruded_scale > 0) {
+		BC_VECTOR n_ex_tmp = BC_EXTRACT_UPPER_DIGIT(*n_ex_ptr, protruded_scale);
+		if (n_ex_tmp != 0) {
+			return bc_compare_determine_return_val(n1->n_scale > n2->n_scale, n1->n_sign, use_sign);
 		}
 	}
-
-	/* They are equal up to the last part of the equal part of the fraction. */
-	if (n1_scale != n2_scale) {
-		if (n1_scale > n2_scale) {
-			for (count = n1_scale - n2_scale; count > 0; count--) {
-				if (*n1ptr++ != 0) {
-					/* Magnitude of n1 > n2. */
-					if (!use_sign || n1->n_sign == PLUS) {
-						return BCMATH_LEFT_GREATER;
-					} else {
-						return BCMATH_RIGHT_GREATER;
-					}
-				}
-			}
-		} else {
-			for (count = n2_scale - n1_scale; count > 0; count--) {
-				if (*n2ptr++ != 0) {
-					/* Magnitude of n1 < n2. */
-					if (!use_sign || n1->n_sign == PLUS) {
-						return BCMATH_RIGHT_GREATER;
-					} else {
-						return BCMATH_LEFT_GREATER;
-					}
-				}
-			}
-		}
-	}
-
-	/* They must be equal! */
 	return BCMATH_EQUAL;
 }
 
